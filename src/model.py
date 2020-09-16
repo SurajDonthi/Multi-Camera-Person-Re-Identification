d74d488f939016e70b7fffd0557e8172f853e26e
SurajDonthi <surajdonthi95@gmail.com>
2020-09-16 17:41:33 +0530

    Created full Pytorch-Lightning Template for Deep Learning

---
+++ src/model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self._layers()
+
+    def _layers(self):
+        pass
+
+    def forward(self, x):
+        pass
+        return x
